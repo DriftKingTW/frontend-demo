0ae9200703596ad1568db22ea777d589885454ef
DriftKingTW <driftkingtw@gmail.com>
2024-04-11 16:00:36 +0800

    fix: base url and router config

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./../views/Home.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/frontend-demo/'),
   routes: [
     {
       path: "/",
